3bbe5a0e7251928ac22290868d3f85197490116d
dianbaer <232365732@qq.com>
2018-03-10 09:47:39 +0800

    11

--- MongoDB/mongodb.sh
+++ MongoDB/mongodb.sh
@@ -1,11 +1,2 @@
-cat << EOF > /etc/yum.repos.d/mongodb.repo
-[mongodb]
-name=mongodb
-baseurl=file:///home/mongodb
-gpgcheck=0
-gpgkey=file:///etc/pki/rpm-gpg/PRM-GPG-KEY-CentOS-7
-enabled=1
-EOF
-
 createrepo /home/mongodb
 
