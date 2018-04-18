cb9aaafd81cb4e78a198c53c9242d3969b65d2e5
dianbaer <232365732@qq.com>
2018-04-18 08:55:57 +0800

    11

--- java/java.sh
+++ java/java.sh
@@ -10,3 +10,13 @@ source /etc/profile
 
 #有一些例如hadoop数据迁移需要bin/java
 ln -s /home/jdk1.8.0_121/bin/java /bin/java
+
+
+JAVA_HOME=/home/jdk1.7.0_80
+PATH=$JAVA_HOME/bin:$PATH
+CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar
+export JAVA_HOME
+export PATH
+export CLASSPATH
+
+ln -s /home/jdk1.7.0_76/bin/java /bin/java
