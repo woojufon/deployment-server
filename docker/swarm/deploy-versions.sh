09e2ad54320f3c21ebd3d86b5f27c3f30ecc3101
xuepeng <232365732@qq.com>
2018-10-23 20:17:22 +0800

    111

---
+++ docker/swarm/deploy-versions.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export TAG_VERSION=1.0.0
+export DATA_PATH=/home
+export IMAGE_PROXY=--with-registry-auth
+
---
+++ docker/swarm/start_base_cluster.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+
+source deploy-versions.sh
+
+#mkdir -p $DATA_PATH/mysql/data
+#mkdir -p $DATA_PATH/mysql/conf
+#mkdir -p $DATA_PATH/redis
+#mkdir -p $DATA_PATH/haproxy/volumes/rmqha_proxy
+
+
+#/usr/bin/docker network create -d overlay spush-net
+
+/usr/bin/docker stack deploy -c docker-base.yml $IMAGE_PROXY swarm-cluster
+
+#sleep 10
+
+#/usr/bin/docker stack deploy -c docker-monitor.yml $IMAGE_PROXY swarm-cluster
+
+
